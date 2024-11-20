05f5bdced73ec4d6da6e8038ed59bacd20b61f5c
rajatkarmokar2 <rajatkarmokar2@gmail.com>
2024-11-20 15:38:35 +0530

    updated tech section to mobile responsive

--- src/components/Tech.jsx
+++ src/components/Tech.jsx
@@ -6,13 +6,13 @@ import { SectionWrapper } from "../hoc";
 
 const Tech = () => {
   return (
-    <Marquee gradient gradientColor='#050816' className='overflow-y-hidden py-3'>
+    <Marquee gradient gradientColor='#050816' gradientWidth='50px' className='overflow-y-hidden py-3'>
       { technologies.map( ( technology ) => (
         <div
-          className="mx-5 hover:scale-110 transition-transform bg-tertiary rounded"
+          className="mx-2 md:mx-5 p-2 hover:scale-110 hover:bg-secondary flex transition-transform bg-tertiary rounded"
           key={ technology.name }
         >
-          <img className='size-28 ' src={ technology.icon } alt='icon' />
+          <img className='size-14 md:size-24 m-auto' src={ technology.icon } alt='icon' />
           {/* <BallCanvas icon={ technology.icon } /> */ }
         </div>
       ) ) }
