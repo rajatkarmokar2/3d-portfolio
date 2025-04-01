c3bd76d8b11304952c893ec8fda264699f9c7f91
rajatkarmokar2 <rajatkarmokar2@gmail.com>
2025-04-01 09:00:00 +0530

    feat: removed project section

--- src/constants/index.js
+++ src/constants/index.js
@@ -35,10 +35,10 @@ export const navLinks = [
     id: "work",
     title: "Work",
   },
-  {
-    id: "projects",
-    title: "Projects",
-  },
+  // {
+  //   id: "projects",
+  //   title: "Projects",
+  // },
   {
     id: "contact",
     title: "Contact",
